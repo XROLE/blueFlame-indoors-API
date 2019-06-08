bb670875c3e84cb7ec06cb7d96cf87ea528e94e7
xrole <xrolediamond@gmail.com>
2019-06-08 22:03:12 +0100

    bg(setup): Fix error with sequelize setup
    - remove models from .gitignore file
    - create model and migration file

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
+    userName: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+  User.associate = () => {
+    // My association code lives here
+  };
+
+  return User;
+};
