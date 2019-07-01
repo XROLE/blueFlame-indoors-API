4ee1c2ecd427005aa123399477124c674c069498
xrole <xrolediamond@gmail.com>
2019-07-01 16:05:27 +0100

    mend

--- server/app.js
+++ server/app.js
@@ -14,11 +14,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('WELCOME TO CURTAIN AND BLIND API VERSION ONE');
 });
 
 app.use('/users', userRoute);
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('App is running on port 3000 .....');
 });
--- server/config/config.js
+++ server/config/config.js
@@ -7,21 +7,21 @@ module.exports = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
+    "username": process.env.DB_USERNAME,
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": 'postgres'
   }
 }
